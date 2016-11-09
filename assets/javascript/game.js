4f8f73d7960534f90e986d099c2c9d09e8027421
Shaverda <shaverda@gmail.com>
2016-11-09 16:57:43 -0600

    finished up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,9 +2,9 @@
 var word_options = ["Aragorn", "Frodo", "Legolas", "Gandalf", "Bilbo", "Sam", "Gimli", "Galadriel", "Pippin", "Arwen", "Gollum", "Elrond", "Saruman", "Merry", "Boromir", "Faramir"]; 
 var secret_word = word_options[Math.floor(Math.random() * word_options.length)].toLowerCase(); //chooses a random secret word from array
 var secret_word_dashed = secret_word.replace(/[a-zA-Z]/g, '-'); //substitutes each char of secret word to a dash
-var stored_guesses = [];
-var guesses_remaining = 10;	
-var userGuess;
+var stored_guesses = []; //THIS STORES GUESSES USED!!!!
+var guesses_remaining = 10;	 //THIS STORES HOW MANY GUESSES YOU HAVE LEFT!!
+var userGuess;	//initiates userGuess to store guessed letter key
 
 function playAgain (){	//asks user if wants to play again; rresets/re-assigns all variables if so
 	var play_again = confirm("Wanna play again?");
@@ -18,7 +18,7 @@ function playAgain (){	//asks user if wants to play again; rresets/re-assigns al
 	 	document.querySelector(".remaining-guesses").innerHTML = guesses_remaining;	//writes how many guesses left onto page
 	}
 	return play_again;	 		
-}
+} 	
 
 window.onload = function gameplay() {
 	document.querySelector(".secret-word-dashed").innerHTML = secret_word_dashed; //rewrites dashed secret word to webpage
@@ -27,11 +27,12 @@ window.onload = function gameplay() {
  		if (guesses_remaining > 0) {
 	 		userGuess = String.fromCharCode(event.keyCode).toLowerCase(); //logs pressed key into userGuess
 	 		stored_guesses.push(userGuess);	//pushes user's guess into stored guesses array
-	 		guesses_remaining--;	//decrements # of guesses
 
 	 		document.querySelector(".stored-letters").innerHTML = stored_guesses; //rewrites letters guessed onto page
 	 		document.querySelector(".remaining-guesses").innerHTML = guesses_remaining;	//writes how many guesses left onto page
-	
+
+		 	guesses_remaining--;	//decrements # of guesses
+
 	 		for (var i = 0; i <secret_word_dashed.length; i++){	//iterates through every char of secret word
 	 			if (secret_word[i] == userGuess) {	//checks if it matches guess
 	 				secret_word_dashed = secret_word_dashed.substring(0,i) + userGuess + secret_word_dashed.substring(i+1); //sets secret_word_dashed to incorporate your proper guessed words by diving into 2 substrings the
